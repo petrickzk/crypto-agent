1e66ff0a677c46379c8e03cb37db5faa8d62a621
petrickzk <zafiraputri866@gmail.com>
2026-06-20 12:26:52 +0700

    Update crypto_agent.py

--- crypto_agent.py
+++ crypto_agent.py
@@ -1,1 +1,26 @@
 print("Halo Petrick!")
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+message = """
+🚀 Halo Petrick!
+
+Crypto Agent berhasil berjalan dari GitHub Actions.
+
+Selamat! Bot Telegram sudah terhubung.
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Pesan berhasil dikirim!")
